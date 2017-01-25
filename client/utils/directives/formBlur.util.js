449f9a76d49ca13a0a86a00f07a64f267a9f5043
Akyuna Akish <akyuna.akish@zayo.com>
2017-01-24 20:39:17 -0700

    testing defocus element to close mobile keyboard"

--- client/utils/directives/formBlur.util.js
+++ client/utils/directives/formBlur.util.js
@@ -1,33 +1,21 @@
-export default () => {
+export default ['$timeout', ($timeout) => {
     return {
         restrict: 'A',
         link: (scope, element) => {
-            let textFields = $(element).children('input[type=text]');
             let defocusElement = angular.element('<input style="opacity: 0; width: 0" type="button">');
             element.append(defocusElement);
 
-            // $(element).submit(() => {
-            //     console.log('submitted');
-            //     textFields.blur();
-            //     document.activeElement.blur();
-            //     $('input').blur();
-            // });
-
-            element.on('submit', () => {
-              console.log('submit...');
-                defocusElement.focus();
-                textFields.blur();
-                $('input').blur();
-            });
-
-            $('body').on('keyup', (event) => {
+            element.on('keydown', (event) => {
+                console.log('on keydown fired', event.keyCode);
                 if (event.keyCode === 13) {
-                    defocusElement.focus();
+                    event.preventDefault();
+                    $timeout(() => {
+                        defocusElement.focus();
+                        element.triggerHandler('submit');
+                    }, 0, false);
                     console.log('keyCode13!!');
-                    textFields.blur();
-                    $('input').blur();
                 }
             });
         }
     };
-};
+}];
