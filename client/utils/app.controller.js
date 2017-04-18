31f41d739009e74cd70a860447fcc8583c28286b
AA <aa@AAs-iMac.local>
2017-04-17 19:08:56 -0700

    Testing open graph image

--- client/utils/app.controller.js
+++ client/utils/app.controller.js
@@ -7,7 +7,7 @@ class AppController {
         if(!this.$rootScope.metadata || Object.keys(this.$rootScope.metadata).length < 1) {
             this.$rootScope.metadata = {
                     title: 'Elena Akish',
-                    image: 'https://drive.google.com/file/d/0Bw6Wv889sj3vUk9ORXFiZW5mZG8/view',
+                    image: 'https://lh6.googleusercontent.com/UTod8SrFSEcCietr_Zp4NOqWU0UEgLLAj9nO3evMsAvnVR5VIuHsXKhOyK5LDhvtQDu2t13I6g939wg=w1024-h581-rw',
                     url: window.location.href
                 };
         }
--- client/utils/stateChange.util.js
+++ client/utils/stateChange.util.js
@@ -16,7 +16,7 @@ export default (['$rootScope', 'authService', '$state', '$localStorage', '$timeo
             } else {
                 $rootScope.metadata = {
                     title: 'Elena Akish',
-                    image: 'https://drive.google.com/file/d/0Bw6Wv889sj3vUk9ORXFiZW5mZG8/view',
+                    image: 'https://lh6.googleusercontent.com/UTod8SrFSEcCietr_Zp4NOqWU0UEgLLAj9nO3evMsAvnVR5VIuHsXKhOyK5LDhvtQDu2t13I6g939wg=w1024-h581-rw',
                     url: window.location.href
                 };
             }
