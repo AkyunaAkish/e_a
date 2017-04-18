1c8c5c972ac461befa577922f66f6dc953a00676
AA <aa@AAs-iMac.local>
2017-04-17 19:16:23 -0700

    default open graph

--- client/utils/stateChange.util.js
+++ client/utils/stateChange.util.js
@@ -1,25 +1,7 @@
-export default (['$rootScope', 'authService', '$state', '$localStorage', '$timeout', '$http', 'HOST',
-    ($rootScope, authService, $state, $localStorage, $timeout, $http, HOST) => {
+export default (['$rootScope', 'authService', '$state', '$localStorage', '$timeout',
+    ($rootScope, authService, $state, $localStorage, $timeout) => {
         $rootScope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams) => {
             $rootScope.$emit('pendingRouteValidation', true);
-
-            if (toParams.id) {
-                $http.get(`${HOST}/posts/retrieve-post/${toParams.id}`).then((post) => {
-                    if (post.data.success) {
-                        $rootScope.metadata = {
-                            title: post.data.success.title,
-                            image: post.data.success.thumbnail_url,
-                            url: `${HOST}/posts/${toParams.id}`
-                        };
-                    }
-                });
-            } else {
-                $rootScope.metadata = {
-                    title: 'Elena Akish',
-                    image: 'https://lh6.googleusercontent.com/UTod8SrFSEcCietr_Zp4NOqWU0UEgLLAj9nO3evMsAvnVR5VIuHsXKhOyK5LDhvtQDu2t13I6g939wg=w1024-h581-rw',
-                    url: window.location.href
-                };
-            }
 
             authService.hasSession()
                 .then((res) => {
